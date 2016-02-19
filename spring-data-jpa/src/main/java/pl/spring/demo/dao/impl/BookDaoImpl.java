60fadf3bff65e9b68be6d593e543e9f2bcefd831
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 15:47:44 +0100

    Change BookTo to BookEntity and write method to find by title and author. Write test and check it out.

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
@@ -1,58 +1,71 @@
 package pl.spring.demo.dao.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.ToDoubleBiFunction;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import pl.spring.demo.annotation.NullableId;
 import pl.spring.demo.dao.BookDao;
-import pl.spring.demo.to.BookTo;
+import pl.spring.demo.entity.AuthorEntity;
+import pl.spring.demo.entity.BookEntity;
 
 @Component
 public class BookDaoImpl implements BookDao {
 
-	private final Set<BookTo> ALL_BOOKS = new HashSet<>();
+	private final Set<BookEntity> ALL_BOOKS = new HashSet<>();
 
 	public BookDaoImpl() {
 		addTestBooks();
 	}
 
 	@Override
-	public List<BookTo> findAll() {
+	public List<BookEntity> findAll() {
 		return new ArrayList<>(ALL_BOOKS);
 	}
 
 	@Override
-	public List<BookTo> findBookByTitle(String title) {
-		return null;
+	public List<BookEntity> findBookByTitle(String title) {
+		return	ALL_BOOKS
+				.stream()
+				.filter((bookEntity)->bookEntity.getTitle().toLowerCase() == title.toLowerCase())
+        		.collect(Collectors.toList());
 	}
 
 	@Override
-	public List<BookTo> findBooksByAuthor(String author) {
-		return null;
+	public List<BookEntity> findBooksByAuthor(String author) {
+		return ALL_BOOKS
+				.stream()
+				.filter((bookEntity)->bookEntity
+					.getAuthors()
+					.stream()
+					.anyMatch((authorTo)->author.contains(authorTo.getFirstName())&&author.contains(authorTo.getLastName())))
+        		.collect(Collectors.toList());
 	}
 
 	@Override
 	@NullableId
-	public BookTo save(BookTo book) {
+	public BookEntity save(BookEntity book) {
 		ALL_BOOKS.add(book);
 		return book;
 	}
 
 	@Override
-	public Set<BookTo> getALL_BOOKS() {
+	public Set<BookEntity> getALL_BOOKS() {
 		return ALL_BOOKS;
 	}
 
 	private void addTestBooks() {
-		ALL_BOOKS.add(new BookTo(1L, "Romeo i Julia", "Wiliam Szekspir"));
-		ALL_BOOKS.add(new BookTo(2L, "Opium w rosole", "Hanna Ożogowska"));
-		ALL_BOOKS.add(new BookTo(3L, "Przygody Odyseusza", "Jan Parandowski"));
-		ALL_BOOKS.add(new BookTo(4L, "Awantura w Niekłaju", "Edmund Niziurski"));
-		ALL_BOOKS.add(new BookTo(5L, "Pan Samochodzik i Fantomas", "Zbigniew Nienacki"));
-		ALL_BOOKS.add(new BookTo(6L, "Zemsta", "Aleksander Fredro"));
+		ALL_BOOKS.add(new BookEntity(1L, "Romeo i Julia", Arrays.asList(new AuthorEntity(1L,"Wiliam", "Szekspir"))));
+		ALL_BOOKS.add(new BookEntity(2L, "Opium w rosole", Arrays.asList(new AuthorEntity(2L,"Hanna", "Ożogowska"))));
+		ALL_BOOKS.add(new BookEntity(3L, "Przygody Odyseusza", Arrays.asList(new AuthorEntity(3L,"Jan", "Parandowski"))));
+		ALL_BOOKS.add(new BookEntity(4L, "Awantura w Niekłaju", Arrays.asList(new AuthorEntity(4L,"Edmund", "Niziurski"))));
+		ALL_BOOKS.add(new BookEntity(5L, "Pan Samochodzik i Fantomas", Arrays.asList(new AuthorEntity(5L,"Zbigniew", "Nienacki"))));
+		ALL_BOOKS.add(new BookEntity(6L, "Zemsta", Arrays.asList(new AuthorEntity(6L,"Aleksander", "Fredro"))));
 	}
 }
