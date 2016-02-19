0db5c0bbed88cd690c610eb6229fef1189f6c332
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 10:43:17 +0100

    Added two autowired instance BookDao, Sequence. Add pointcut for save method in BookDao. Add checking id.

--- spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
@@ -4,29 +4,59 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import pl.spring.demo.common.Sequence;
+import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.exception.BookNotNullIdException;
+import pl.spring.demo.to.BookTo;
 import pl.spring.demo.to.IdAware;
 
 @Component
 @Aspect
 public class BookDaoAdvisor {
+	
+	@Autowired
+	private Sequence sequence;
+	
+	@Autowired
+	private BookDao bookDao;
+	
 
 	@Pointcut("@annotation(pl.spring.demo.annotation.NullableId)")
-	public void pointCutForNullableId() {
-		System.out.println("Hello");
-	};
+	public void pointCutForNullableIdAnnotation() {
+	}
 
-	@Before("pointCutForNullableId()")
+	@Before("pointCutForNullableIdAnnotation()")
 	public void beforeNullableId(JoinPoint joinPoint) {
 		checkNotNullId(joinPoint.getArgs()[0]);
 	}
-
+	
 	private void checkNotNullId(Object o) {
 		if (o instanceof IdAware && ((IdAware) o).getId() != null) {
 			throw new BookNotNullIdException();
 		}
+	}
+	
+	@Pointcut("execution(public * pl.spring.demo.dao.BookDao.save(pl.spring.demo.to.BookTo))")
+	public void setIdForSaveBook(){
+	}
+	
+	@Before("setIdForSaveBook()")
+	public void setId(JoinPoint joinPoint){
+		BookTo book = (BookTo) joinPoint.getArgs()[0];
+		if (book.getId() == null) {
+			book.setId(sequence.nextValue(bookDao.getALL_BOOKS()));
+		}
+	}
+	
+	public void setSequence(Sequence sequence) {
+		this.sequence = sequence;
+	}
+
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
 	}
 
 }
