73052891d5d9336425e02ceaf9235f27f60d1d9e
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 15:51:08 +0100

    Mapping BookEntity to BookTo and adding finding by title and author

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -4,40 +4,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import pl.spring.demo.bookmapper.BookMapper;
 import pl.spring.demo.dao.BookDao;
+import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class BookServiceImpl implements BookService {
 
 	@Autowired
     private BookDao bookDao;
+	
+	@Autowired
+	private BookMapper bookMapper;
 
     @Override
     @Cacheable("booksCache")
     public List<BookTo> findAllBooks() {
-        return bookDao.findAll();
+        return bookDao.findAll().stream().map((bookEntity)->bookMapper.bookEntityToBookTo(bookEntity)).collect(Collectors.toList());
     }
 
     @Override
     public List<BookTo> findBooksByTitle(String title) {
-        return bookDao.findBookByTitle(title);
+        return bookDao.findBookByTitle(title).stream().map((bookEntity)->bookMapper.bookEntityToBookTo(bookEntity)).collect(Collectors.toList());
     }
 
     @Override
     public List<BookTo> findBooksByAuthor(String author) {
-        return bookDao.findBooksByAuthor(author);
+        return bookDao.findBooksByAuthor(author).stream().map((bookEntity)->bookMapper.bookEntityToBookTo(bookEntity)).collect(Collectors.toList());
+        		
     }
 
     @Override
     public BookTo saveBook(BookTo book) {
-        return bookDao.save(book);
+    	BookEntity bookToToBookEntity = bookMapper.bookToToBookEntity(book);
+        BookEntity bookEntity = bookDao.save(bookToToBookEntity);
+        return bookMapper.bookEntityToBookTo(bookEntity);
     }
 
     public void setBookDao(BookDao bookDao) {
         this.bookDao = bookDao;
     }
+
+	public void setBookMapper(BookMapper bookMapper) {
+		this.bookMapper = bookMapper;
+	}
+    
+    
 }
