a8f9b05cb2ad58652a9627ba75618553ef39494c
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 15:46:26 +0100

    Change using BookTo to BookEnity.

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/BookDao.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/BookDao.java
@@ -1,5 +1,6 @@
 package pl.spring.demo.dao;
 
+import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.to.BookTo;
 
 import java.util.List;
@@ -7,13 +8,13 @@ import java.util.Set;
 
 public interface BookDao {
 
-    List<BookTo> findAll();
+    List<BookEntity> findAll();
 
-    List<BookTo> findBookByTitle(String title);
+    List<BookEntity> findBookByTitle(String title);
 
-    List<BookTo> findBooksByAuthor(String author);
+    List<BookEntity> findBooksByAuthor(String author);
 
-    BookTo save(BookTo book);
+    BookEntity save(BookEntity book);
     
-    Set<BookTo> getALL_BOOKS();
+    Set<BookEntity> getALL_BOOKS();
 }
