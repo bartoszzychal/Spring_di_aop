3b51cc968bda2d534672cfb216088d19791a092b
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 08:28:40 +0100

    Added annotation @Service for Class and @Autowired for BookDao instance.

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -1,14 +1,19 @@
 package pl.spring.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
 import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
 import java.util.List;
 
+@Service
 public class BookServiceImpl implements BookService {
 
+	@Autowired
     private BookDao bookDao;
 
     @Override
