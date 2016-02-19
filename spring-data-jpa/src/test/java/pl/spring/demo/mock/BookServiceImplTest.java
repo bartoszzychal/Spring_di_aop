27fca70ca13edeb8d0c1e21b775c3e398c315619
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-19 15:51:58 +0100

    Changes BookTo on BookEntity

--- spring-data-jpa/src/test/java/pl/spring/demo/mock/BookServiceImplTest.java
+++ spring-data-jpa/src/test/java/pl/spring/demo/mock/BookServiceImplTest.java
@@ -1,16 +1,21 @@
 package pl.spring.demo.mock;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
 import pl.spring.demo.dao.BookDao;
+import pl.spring.demo.entity.AuthorEntity;
+import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.service.impl.BookServiceImpl;
 import pl.spring.demo.to.BookTo;
-
-import static org.junit.Assert.assertEquals;
 
 public class BookServiceImplTest {
 
@@ -28,11 +33,12 @@ public class BookServiceImplTest {
     public void testShouldSaveBook() {
         // given
         BookTo book = new BookTo(null, "title", "author");
-        Mockito.when(bookDao.save(book)).thenReturn(new BookTo(1L, "title", "author"));
+        BookEntity bookEntity = new BookEntity(null, "title", Arrays.asList(new AuthorEntity(1L,"author","")));
+        Mockito.when(bookDao.save(bookEntity)).thenReturn(new BookEntity(1L, "title", Arrays.asList(new AuthorEntity(1L,"author",""))));
         // when
         BookTo result = bookService.saveBook(book);
         // then
-        Mockito.verify(bookDao).save(book);
+        Mockito.verify(bookDao).save(bookEntity);
         assertEquals(1L, result.getId().longValue());
     }
 }
