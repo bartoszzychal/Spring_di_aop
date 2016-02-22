86b6cbd70eda989112bd6f409fe2be78c4ae774c
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-22 14:20:55 +0100

    Extract mapping authors to AuthorsMapper class.

--- spring-data-jpa/src/main/java/pl/spring/demo/bookmapper/BookMapper.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/bookmapper/BookMapper.java
@@ -1,36 +1,23 @@
 package pl.spring.demo.bookmapper;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import org.springframework.stereotype.Component;
-
-import pl.spring.demo.entity.AuthorEntity;
+import pl.spring.demo.authormapper.AuthorsMapper;
 import pl.spring.demo.entity.BookEntity;
 import pl.spring.demo.to.BookTo;
 
-@Component
+@Service
 public class BookMapper {
 	
-	private final String REGEX_FIRSTNAME_LASTNAME = " ";
-	private final String REGEX_AUTHOR_AUTHOR = ",";
-		
+	@Autowired
+	AuthorsMapper authorsMapper;
+	
 	public BookEntity bookToToBookEntity(BookTo bookTo){
 		BookEntity bookEntity = new BookEntity();
 		bookEntity.setId(bookTo.getId());
-		bookEntity.setTitle(bookTo.getTitle());
-
-		List<AuthorEntity> authors = Arrays.asList(bookTo.getAuthors().split(REGEX_AUTHOR_AUTHOR))
-				.stream()
-				.map((author) -> {
-					List<String> firstLastName = Arrays.asList(author.split(REGEX_FIRSTNAME_LASTNAME));
-					return new AuthorEntity(1L, firstLastName.get(0).trim(), firstLastName.get(1).trim());
-				}).collect(Collectors.toList());
-		
-		bookEntity.setAuthors(authors);
-		
+		bookEntity.setTitle(bookTo.getTitle());		
+		bookEntity.setAuthors(authorsMapper.stringToListOfAuthorEntity(bookTo.getAuthors()));
 		return bookEntity;
 	}
 	
@@ -38,14 +25,8 @@ public class BookMapper {
 		BookTo bookTo = new BookTo();
 		bookTo.setId(bookEntity.getId());
 		bookTo.setTitle(bookEntity.getTitle());
-		
-		bookTo.setAuthors(
-				bookEntity
-				.getAuthors()
-				.stream()
-				.map(author->author.getFirstName()+REGEX_FIRSTNAME_LASTNAME+author.getLastName())
-				.reduce((s1,s2)-> s1+ REGEX_AUTHOR_AUTHOR + s2).get());
-				
+		bookTo.setAuthors(authorsMapper.listOfAuthorEntityToString(bookEntity.getAuthors()));
 		return bookTo;
 	}
+
 }
