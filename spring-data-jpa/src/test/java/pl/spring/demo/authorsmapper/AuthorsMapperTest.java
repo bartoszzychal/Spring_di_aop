db057e85a1ee57e46a9557848df80c3af8e9288d
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-22 14:16:04 +0100

    Create AuthorsMapperTest. Testing parse String to AuthorEntity for one and many authors. Testing AuthorEntity to String for one and many authors. First and Last name are separated by space. Authors are separated by comma.

---
+++ spring-data-jpa/src/test/java/pl/spring/demo/authorsmapper/AuthorsMapperTest.java
@@ -0,0 +1,79 @@
+package pl.spring.demo.authorsmapper;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import pl.spring.demo.authormapper.AuthorsMapper;
+import pl.spring.demo.entity.AuthorEntity;
+
+public class AuthorsMapperTest {
+	
+	@InjectMocks
+	AuthorsMapper authorMapper;
+	
+	@Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	@Test
+	public void shouldReturnStringForAuthorEntity(){
+		//given
+		List<AuthorEntity> author = Arrays.asList(new AuthorEntity(1L,"firstName","lastName"));
+		//expected
+		String expected = "firstName lastName";
+		//when
+		String actual = authorMapper.listOfAuthorEntityToString(author);
+		//then
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void shouldReturnListOfAuthorEntityForString(){
+		//given
+		String author = "firstName lastName";
+		//expected
+		List<AuthorEntity> expected = Arrays.asList(new AuthorEntity(1L,"firstName","lastName"));
+		//when
+		List<AuthorEntity> actual = authorMapper.stringToListOfAuthorEntity(author);
+		//then
+		assertEquals(expected.get(0).getFirstName(), actual.get(0).getFirstName());
+		assertEquals(expected.get(0).getLastName(), actual.get(0).getLastName());
+	}
+	
+	@Test
+	public void shouldReturnStringForAuthorEntities(){
+		//given
+		List<AuthorEntity> authors = Arrays.asList(new AuthorEntity(1L,"firstName","lastName"),new AuthorEntity(1L,"firstName","lastName") );
+		//expected
+		String expected = "firstName lastName, firstName lastName";
+		//when
+		String actual = authorMapper.listOfAuthorEntityToString(authors);
+		//then
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void shouldReturnListOfAuthorEntityForStringWithTwoAuthors(){
+		//given
+		String authors = "firstName lastName, firstName lastName";
+		//expected
+		List<AuthorEntity> expected = Arrays.asList(new AuthorEntity(1L,"firstName","lastName"),new AuthorEntity(1L,"firstName","lastName") );
+		//when
+		List<AuthorEntity> actual = authorMapper.stringToListOfAuthorEntity(authors);
+		//then
+		assertTrue(actual.size() == 2);
+		assertEquals(expected.get(0).getFirstName(), actual.get(0).getFirstName());
+		assertEquals(expected.get(0).getLastName(), actual.get(0).getLastName());
+		assertEquals(expected.get(1).getFirstName(), actual.get(1).getFirstName());
+		assertEquals(expected.get(1).getLastName(), actual.get(1).getLastName());
+	}
+	
+}
